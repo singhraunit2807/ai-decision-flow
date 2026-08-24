129b4342d1f0e63da410cbf8fc6e9c1a70b08bbb
Raunit Singh <baburajasingh7484@gmail.com>
2026-08-25 01:55:48 +0530

    Add NOVA speech-to-text adapter

---
+++ nova/voice/stt.py
@@ -0,0 +1,6 @@
+def transcribe_audio(audio_path: str) -> str:
+    """Placeholder STT adapter.
+
+    A production version can call Whisper or another speech-to-text provider.
+    """
+    raise NotImplementedError("Connect Whisper/STT provider here")
